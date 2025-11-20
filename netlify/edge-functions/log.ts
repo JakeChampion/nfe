9586f65817735a30f6c6fcefad92fd84cb2ea8bf
Jake Champion <me@jakechampion.name>
2025-11-20 15:31:26 +0000

    Refactor log function to remove context parameter

--- netlify/edge-functions/log.ts
+++ netlify/edge-functions/log.ts
@@ -1,6 +1,6 @@
-import type { Context, Config } from "@netlify/edge-functions";
+import type { Config } from "@netlify/edge-functions";
 
-export default async (request: Request, context: Context) => {
+export default async () => {
   console.log("Hello from the logging service");
 
   return new Response("The request to this URL was logged");
